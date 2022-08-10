7187b5b68bdec5637e9de0cc2e560c5120a757ae
OmarMWarraich <earthling986@gmail.com>
2022-08-10 21:46:24 +0500

    fix: remove linter errors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+// eslint-disable-next-line max-classes-per-file
 class Collection {
+  // eslint-disable-next-line no-unused-vars
   constructor(books) {
     this.books = [];
   }
 
   add(book) {
     this.books.push(book);
-  }  
+  }
 
   remove(book) {
-    this.books = this.books.filter(i => i !== book);
+    this.books = this.books.filter((i) => i !== book);
   }
 
   get(index) {
@@ -16,6 +18,7 @@ class Collection {
   }
 }
 
+// eslint-disable-next-line max-classes-per-file
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -23,41 +26,37 @@ class Book {
   }
 }
 
-let myCollection = new Collection();
+const myCollection = new Collection();
 
 const addbtn = document.querySelector('.btn');
-  addbtn.addEventListener('click', (e) => {
-    const title = document.querySelector('.title').value;
-    const author = document.querySelector('.author').value;
-    let newBook = new Book(title, author);
+addbtn.addEventListener('click', (e) => {
+  const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
+  if (title === '' || author === '') {
+    // eslint-disable-next-line no-alert
+    alert('Please fill in all fields');
+  } else {
+    const newBook = new Book(title, author);
     myCollection.add(newBook);
     const display = document.getElementById('book-list');
     const div = document.createElement('div');
     div.classList = 'book';
     div.innerHTML = `<h2>${newBook.title}</h2><p>${newBook.author}</p><button class="remove">Remove</button>`;
     display.appendChild(div);
-    
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
     e.preventDefault();
-
-    // console.log(myCollection);
-    // localStorage.setItem('myCollection', JSON.stringify(myCollection.books));
+    localStorage.setItem('myCollection', JSON.stringify(myCollection.books));
     const deletebtn = document.querySelectorAll('.remove');
-    console.log(deletebtn);
-    deletebtn.forEach(btn => {
+
+    deletebtn.forEach((btn) => {
       btn.addEventListener('click', (e) => {
-        
-        const book = e.target.parentElement;      
+        const book = e.target.parentElement;
         myCollection.remove(book);
         book.remove();
-        // localStorage.removeItem('myCollection');
-        e.preventDefault();    
+        localStorage.removeItem('myCollection');
+        e.preventDefault();
       });
     });
-    title.value = '';
-    author.value = '';
+  }
 });
-// });
-// const getCollection = JSON.parse(localStorage.getItem('myCollection'));
-// console.log(getCollection);
-
-
