9e8b6347a2d2db3cc1086614054e390f1df52ae2
OmarMWarraich <earthling986@gmail.com>
2022-08-11 22:21:50 +0500

    feat: Add dynamic elements functionality

--- index.js
+++ index.js
@@ -28,15 +28,11 @@ class Book {
 
 const date_time = document.querySelector('.date-time');
 date_time.innerHTML = new Date().toLocaleString();
-
-
 const list = document.getElementById('list');
 const addList = document.getElementById('add');
 const contact = document.getElementById('contact');
 const displaySection = document.getElementById('book-list');
 const inputSection = document.querySelector('.book-input');
-
-
 const contactSection = document.querySelector('.contact');
 
 const myCollection = new Collection();
@@ -46,29 +42,6 @@ list.addEventListener('click', (e) => {
   displaySection.style.display = 'block';
   inputSection.style.display = 'none';
   contactSection.style.display = 'none';
-
-  
-  const storedBooks = JSON.parse(localStorage.getItem('Collection'));
-  if (storedBooks) {
-    storedBooks.forEach((book) => {
-      const display = document.getElementById('book-list');
-      const div = document.createElement('div');
-      div.classList = 'book';
-      div.innerHTML = `<div>"${book.title}" by ${book.author}</div><div><button class="remove">Remove</button></div>`;
-      display.appendChild(div);
-      e.preventDefault();
-    });
-  }
-  const deletebtn = document.querySelectorAll('.remove');
-
-  deletebtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      const book = e.target.parentElement.parentElement;
-      myCollection.remove(book);
-      book.remove();
-      e.preventDefault();
-    });
-  });
 });
 
 addList.addEventListener('click', (e) => {
@@ -84,82 +57,49 @@ addList.addEventListener('click', (e) => {
                           <button type="submit" class="btn">Add</button>
 `;
   e.preventDefault();
+  const addbtn = document.querySelector('.btn');
+  addbtn.addEventListener('click', (e) => {
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    if (title === '' || author === '') {
+      // eslint-disable-next-line no-alert
+      alert('Please fill in all fields');
+    } else {
+      const newBook = new Book(title, author);
+      myCollection.add(newBook);
+      const display = document.getElementById('book-list');
+      const div = document.createElement('div');
+      div.classList = 'book';
+      div.innerHTML = `<div>"${newBook.title}" by ${newBook.author}</div><div><button class="remove">Remove</button></div>`;
+      display.appendChild(div);
+      document.querySelector('.title').value = '';
+      document.querySelector('.author').value = '';
+      e.preventDefault();
+      localStorage.setItem('Collection', JSON.stringify(myCollection.books));
+
+      const deletebtn = document.querySelectorAll('.remove');
+
+      deletebtn.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+          const book = e.target.parentElement.parentElement;
+          myCollection.remove(book);
+          book.remove();
+          e.preventDefault();
+        });
+      });
+    }
 });
   
-  contact.addEventListener('click', (e) => {
-    displaySection.style.display = 'none';
-    inputSection.style.display = 'none';
-    contactSection.style.display = 'block';
-    contactSection.innerHTML = `<h1>Contact</h1><p>Hello there!<p>`;
-    e.preventDefault();
-  } );
-
-
-  /* const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
-  if (title === '' || author === '') {
-    // eslint-disable-next-line no-alert
-    alert('Please fill in all fields');
-  } else {
-    const newBook = new Book(title, author);
-    myCollection.add(newBook);
-    // const display = document.getElementById('book-list');
-    const div = document.createElement('div');
-    div.classList = 'book';
-    div.innerHTML = `<div>"${newBook.title}" by ${newBook.author}</div><div><button class="remove">Remove</button></div>`;
-    display.appendChild(div);
-    document.querySelector('.title').value = '';
-    document.querySelector('.author').value = '';
-    e.preventDefault();
-    localStorage.setItem('Collection', JSON.stringify(myCollection.books));
-
-    const deletebtn = document.querySelectorAll('.remove');
-
-    deletebtn.forEach((btn) => {
-      btn.addEventListener('click', (e) => {
-        const book = e.target.parentElement.parentElement;
-        myCollection.remove(book);
-        book.remove();
-        e.preventDefault();
-      });
-    });
-  } */
-
-
-
-
-/* const addbtn = document.querySelector('.btn');
-addbtn.addEventListener('click', (e) => {
-  const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
-  if (title === '' || author === '') {
-    // eslint-disable-next-line no-alert
-    alert('Please fill in all fields');
-  } else {
-    const newBook = new Book(title, author);
-    myCollection.add(newBook);
-    const display = document.getElementById('book-list');
-    const div = document.createElement('div');
-    div.classList = 'book';
-    div.innerHTML = `<div>"${newBook.title}" by ${newBook.author}</div><div><button class="remove">Remove</button></div>`;
-    display.appendChild(div);
-    document.querySelector('.title').value = '';
-    document.querySelector('.author').value = '';
-    e.preventDefault();
-    localStorage.setItem('Collection', JSON.stringify(myCollection.books));
-
-    const deletebtn = document.querySelectorAll('.remove');
-
-    deletebtn.forEach((btn) => {
-      btn.addEventListener('click', (e) => {
-        const book = e.target.parentElement.parentElement;
-        myCollection.remove(book);
-        book.remove();
-        e.preventDefault();
-      });
-    });
-  }
 });
+  
+contact.addEventListener('click', (e) => {
+  displaySection.style.display = 'none';
+  inputSection.style.display = 'none';
+  date_time.style.display = 'none';
+  contactSection.style.display = 'block';
+  contactSection.innerHTML = `<h1>Contact</h1><p>Hello there!<p>`;
+  e.preventDefault();
+} );
 
 window.onload = () => {
   const storedBooks = JSON.parse(localStorage.getItem('Collection'));
@@ -173,7 +113,6 @@ window.onload = () => {
     });
   }
   const deletebtn = document.querySelectorAll('.remove');
-
   deletebtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       const book = e.target.parentElement.parentElement;
@@ -183,4 +122,3 @@ window.onload = () => {
     });
   });
 };
-*/
