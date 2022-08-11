a439fd0cbd045c255f16fc028b11b5141c87cbb4
OmarMWarraich <earthling986@gmail.com>
2022-08-11 21:12:48 +0500

    feat: Add content dynamic html

--- index.js
+++ index.js
@@ -30,9 +30,105 @@ const date_time = document.querySelector('.date-time');
 date_time.innerHTML = new Date().toLocaleString();
 
 
+const list = document.getElementById('list');
+const addList = document.getElementById('add');
+const contact = document.getElementById('contact');
+const displaySection = document.getElementById('book-list');
+const inputSection = document.querySelector('.book-input');
+
+
+const contactSection = document.querySelector('.contact');
+
 const myCollection = new Collection();
 
-const addbtn = document.querySelector('.btn');
+
+list.addEventListener('click', (e) => {
+  displaySection.style.display = 'block';
+  inputSection.style.display = 'none';
+  contactSection.style.display = 'none';
+
+  
+  const storedBooks = JSON.parse(localStorage.getItem('Collection'));
+  if (storedBooks) {
+    storedBooks.forEach((book) => {
+      const display = document.getElementById('book-list');
+      const div = document.createElement('div');
+      div.classList = 'book';
+      div.innerHTML = `<div>"${book.title}" by ${book.author}</div><div><button class="remove">Remove</button></div>`;
+      display.appendChild(div);
+      e.preventDefault();
+    });
+  }
+  const deletebtn = document.querySelectorAll('.remove');
+
+  deletebtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const book = e.target.parentElement.parentElement;
+      myCollection.remove(book);
+      book.remove();
+      e.preventDefault();
+    });
+  });
+});
+
+addList.addEventListener('click', (e) => {
+  displaySection.style.display = 'none';
+  contactSection.style.display = 'none';
+  inputSection.style.display = 'block';
+  inputSection.innerHTML = `
+                          <br>
+                          <input type="text" name="title"  class ="title" placeholder="Title" required>
+                          <br><br>
+                          <input type="text" name="title"  class="author" placeholder="Author" required>
+                          <br><br>
+                          <button type="submit" class="btn">Add</button>
+`;
+  e.preventDefault();
+});
+  
+  contact.addEventListener('click', (e) => {
+    displaySection.style.display = 'none';
+    inputSection.style.display = 'none';
+    contactSection.style.display = 'block';
+    contactSection.innerHTML = `<h1>Contact</h1><p>Hello there!<p>`;
+    e.preventDefault();
+  } );
+
+
+  /* const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
+  if (title === '' || author === '') {
+    // eslint-disable-next-line no-alert
+    alert('Please fill in all fields');
+  } else {
+    const newBook = new Book(title, author);
+    myCollection.add(newBook);
+    // const display = document.getElementById('book-list');
+    const div = document.createElement('div');
+    div.classList = 'book';
+    div.innerHTML = `<div>"${newBook.title}" by ${newBook.author}</div><div><button class="remove">Remove</button></div>`;
+    display.appendChild(div);
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+    e.preventDefault();
+    localStorage.setItem('Collection', JSON.stringify(myCollection.books));
+
+    const deletebtn = document.querySelectorAll('.remove');
+
+    deletebtn.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const book = e.target.parentElement.parentElement;
+        myCollection.remove(book);
+        book.remove();
+        e.preventDefault();
+      });
+    });
+  } */
+
+
+
+
+/* const addbtn = document.querySelector('.btn');
 addbtn.addEventListener('click', (e) => {
   const title = document.querySelector('.title').value;
   const author = document.querySelector('.author').value;
@@ -87,3 +183,4 @@ window.onload = () => {
     });
   });
 };
+*/
